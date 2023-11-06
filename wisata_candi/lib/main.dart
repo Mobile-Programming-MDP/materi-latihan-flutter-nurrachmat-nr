bb000d5f9d32a786fc63e90064588aa1155f4447
Yohannes Masterous <>
2023-11-06 18:33:03 +0700

    SearchScreen

--- wisata_candi/lib/main.dart
+++ wisata_candi/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:wisata_candi/data/candi_data.dart';
 //import 'package:wisata_candi/screens/detail_screen.dart';
 import 'package:wisata_candi/screens/profile_screen.dart';
+import 'package:wisata_candi/screens/search_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,7 +20,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: ProfileScreen(),
+      home: SearchScreen(),
+      //home: ProfileScreen(),
       //home: DetailScreen(candi: candiList[0]),
     );
   }
