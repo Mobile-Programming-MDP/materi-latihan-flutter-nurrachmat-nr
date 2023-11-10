3216a6089f37dc064050f3f7afb44c0f41b43ebf
Nur Rachmat <rachmat.nur91@gmail.com>
2023-11-10 18:02:39 +0700

    Home Screen

--- wisata_candi/lib/main.dart
+++ wisata_candi/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:wisata_candi/data/candi_data.dart';
+import 'package:wisata_candi/screens/home_screen.dart';
 //import 'package:wisata_candi/screens/detail_screen.dart';
 import 'package:wisata_candi/screens/profile_screen.dart';
 import 'package:wisata_candi/screens/search_screen.dart';
@@ -20,7 +21,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: SearchScreen(),
+      home: HomeScreen(),
+      // home: SearchScreen(),
       //home: ProfileScreen(),
       //home: DetailScreen(candi: candiList[0]),
     );
